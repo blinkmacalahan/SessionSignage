d7e6498a34b9202c6ca777dee406a1bb6cce8251
Justin Carstens <jcarstens@cvent.com>
2021-02-23 12:06:03 -0700

    Finish Initial Setup

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -7,6 +7,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Inital load of the data
+        SignageSDK.shared.loadSessionOverviews()
+        
         return true
     }
 
